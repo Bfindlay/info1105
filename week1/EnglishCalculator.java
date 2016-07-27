62d462d65520d704f7ad4990aa4d40cd1bc9d159
bfindlay <Brett@MacBook-Air.local>
2016-07-27 10:51:53 +1000

    first challenge week 1

---
+++ week1/EnglishCalculator.java
@@ -0,0 +1,26 @@
+public class EnglishCalculator {
+
+	public static double calculate(String s) {
+		String[] sp = s.split(" ");
+		double num1 = Double.parseDouble(sp[0]);
+		double num2 = (sp.length == 3) ? Double.parseDouble(sp[2]) : Double.parseDouble(sp[3]);
+		switch (sp[1]) {
+		case "times":
+			return num1 * num2;
+		case "plus":
+			return num1 + num2;
+
+		case "minus":
+			return num1 - num2;
+		case "divided":
+			if (num2 != 0)
+				return (num1 / num2);
+			else
+				throw new IllegalArgumentException("cannot divide by zero");
+		default:
+			throw new IllegalArgumentException("not a valid operation");
+		}
+
+	}
+
+}
---
+++ week1/EnglishCalculatorTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EnglishCalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		double t1 = EnglishCalculator.calculate("1 plus 2");
+		double t2 = EnglishCalculator.calculate("1 divided by 2");
+		double t6 = EnglishCalculator.calculate("1 divided by 2");
+		double t3 = EnglishCalculator.calculate("1 times 2");
+		double t4 = EnglishCalculator.calculate("1 minus 2");
+		// double t5 = EnglishCalculator.calculate("1 divided 0");
+
+		assertEquals(3, t1, 0001);
+		assertEquals(0.5, t2, 0001);
+		assertEquals(2, t3, 0001);
+		assertEquals(-1, t4, 0001);
+		assertEquals(0.5, t6, 0001);
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testException() {
+		double t0 = EnglishCalculator.calculate("1 divided 0");
+	}
+
+}
