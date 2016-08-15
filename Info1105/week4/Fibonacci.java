c390d29553b81447dc7c046725f1f5a2185887ef
bfindlay <bfin5330@uni.sydney.edu.au>
2016-08-15 16:59:12 +1000

    implementing week 4 fib

--- Info1105/week4/Fibonacci.java
+++ Info1105/week4/Fibonacci.java
@@ -1,5 +1,16 @@
 package week4;
 
+import java.util.Stack;
+
 public class Fibonacci {
 
+	public static Stack<Integer> getNumbers(int n) {
+		Stack<Integer> s = new Stack<>();
+
+		return s;
+	}
+
+	public static void main(String[] args) {
+
+	}
 }
