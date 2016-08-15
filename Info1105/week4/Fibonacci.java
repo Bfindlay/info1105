f2c248fa7954c8e4c32a63e0816da74e6660c742
bfindlay <bfin5330@uni.sydney.edu.au>
2016-08-15 17:38:53 +1000

    finished fibonacci stack

--- Info1105/week4/Fibonacci.java
+++ Info1105/week4/Fibonacci.java
@@ -6,11 +6,18 @@ public class Fibonacci {
 
 	public static Stack<Integer> getNumbers(int n) {
 		Stack<Integer> s = new Stack<>();
-
+		int x = 0, y = 0, z = 1;
+		for (int i = 0; i < n + 1; i++) {
+			x = y;
+			y = z;
+			z = x + y;
+			s.push(x);
+		}
 		return s;
 	}
 
 	public static void main(String[] args) {
-
+		Stack<Integer> test = getNumbers(3);
+		System.out.println(test);
 	}
 }
