47c1480c3800607cc57aa3f96396df5cd22983ae
bfindlay <bfin5330@uni.sydney.edu.au>
2016-08-31 22:35:33 +1000

    implementing tests for challenges 1 & 2

--- Info1105/week6/ArrayHeapCheckerTest.java
+++ Info1105/week6/ArrayHeapCheckerTest.java
@@ -27,4 +27,23 @@ public class ArrayHeapCheckerTest {
 		assertFalse(ArrayHeapChecker.isBinaryTree(new Integer[] { null, 0 }));
 	}
 
+	@Test
+	public void testComplete() {
+		assertTrue(ArrayHeapChecker.isCompleteBinaryTree(new Integer[] { 1, 2, 5, 6, -1, 8, 11 }));
+		// assertFalse(ArrayHeapChecker.isCompleteBinaryTree(new Integer[] { 1,
+		// 2, 5, null, -1, 8, 11 }));
+
+		assertFalse(ArrayHeapChecker.isCompleteBinaryTree(new Integer[] { 1, null, 5, 6, -18, 11 }));
+
+		assertFalse(ArrayHeapChecker.isCompleteBinaryTree(new Integer[] { 1, 2, null, 6, -1, 8, 11 }));
+
+		assertTrue(ArrayHeapChecker.isCompleteBinaryTree(new Integer[] { null }));
+
+		assertTrue(ArrayHeapChecker.isCompleteBinaryTree(new Integer[] { null, null, null }));
+
+		assertTrue(ArrayHeapChecker.isBinaryTree(new Integer[] { 0, 1, 2, 3, 4, 5, 6 }));
+		// assertFalse(ArrayHeapChecker.isCompleteBinaryTree(new Integer[] { 0,
+		// null, 0 }));
+	}
+
 }
