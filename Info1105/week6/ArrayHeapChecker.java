8490ad2838e2d89d391b216fe66d3036b4906067
bfindlay <bfin5330@uni.sydney.edu.au>
2016-08-31 19:04:27 +1000

    implemented first challenge

--- Info1105/week6/ArrayHeapChecker.java
+++ Info1105/week6/ArrayHeapChecker.java
@@ -4,13 +4,15 @@ public class ArrayHeapChecker {
 
 	// Check if the given array is a representation of a binary tree
 	public static boolean isBinaryTree(Integer[] array) {
-		// TODO: implement this
-        if(array.length %2 == 0)
-             return true;
-        else
-       return false;
-    }
-
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] != null) {
+				int parent = (i - 1) / 2;
+				if (array[parent] == null)
+					return false;
+			}
+		}
+		return true;
+	}
 
 	// Check if the given array is a complete binary tree
 	public static boolean isCompleteBinaryTree(Integer[] array) {
@@ -27,7 +29,7 @@ public class ArrayHeapChecker {
 			return false;
 		}
 		// TODO: implement this
-		return true
-	
-        
+		return true;
+	}
+
 }
---
+++ Info1105/week6/ArrayHeapCheckerTest.java
@@ -0,0 +1,30 @@
+package week6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArrayHeapCheckerTest {
+
+	@Test(timeout = 100)
+	public void testIsBinaryTree_ExampleTest() {
+
+		// These are all binary trees
+		assertTrue(ArrayHeapChecker.isBinaryTree(new Integer[] { 0, 1, 2, 3, 4, 5, 6 }));
+		assertTrue(ArrayHeapChecker.isBinaryTree(new Integer[] { 8, 8, 8, 8, 8, 8, 8 }));
+		assertTrue(ArrayHeapChecker.isBinaryTree(new Integer[] { 0, 1, null, 3, 4, null, null }));
+		assertTrue(ArrayHeapChecker.isBinaryTree(new Integer[] { 0, 1, 2, null, 4, null, 6 }));
+		assertTrue(ArrayHeapChecker.isBinaryTree(new Integer[] { 0, null, null }));
+		// assertTrue(ArrayHeapChecker.isBinaryTree(new Integer[] { 0 }));
+		assertTrue(ArrayHeapChecker.isBinaryTree(new Integer[] { null }));
+		assertTrue(ArrayHeapChecker.isBinaryTree(new Integer[] {}));
+
+		// These are not binary trees (because nodes exist which don't have
+		// parents)
+		assertFalse(ArrayHeapChecker.isBinaryTree(new Integer[] { null, 1, 2, 3, 4, 5, 6 }));
+		assertFalse(ArrayHeapChecker.isBinaryTree(new Integer[] { 0, 1, null, 3, 4, 5, null }));
+		assertFalse(ArrayHeapChecker.isBinaryTree(new Integer[] { 0, 1, null, null, 4, 5, 6 }));
+		assertFalse(ArrayHeapChecker.isBinaryTree(new Integer[] { null, 0 }));
+	}
+
+}
