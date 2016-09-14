f19f845ebbe7be9577eade687d5f27f964ad9bf5
bfindlay <bfin5330@uni.sydney.edu.au>
2016-09-15 03:58:12 +1000

    started week 8 work

---
+++ Info1105/week8/TravelDestinations.java
@@ -0,0 +1,56 @@
+package week8;
+
+import java.util.List;
+
+public class TravelDestinations {
+
+	private Graph<String, Integer> graph;
+
+	public TravelDestinations(Graph<String, Integer> graph) {
+		this.graph = graph;
+		// TODO: implement this method
+	}
+
+	/* Exercise 1 methods */
+
+	/**
+	 * Return all the countries that are a single direct flight away from the
+	 * given country, in any order. If no flights depart this country, return an
+	 * empty List.
+	 */
+	public List<String> getDirectDestinations(String fromCountry) {
+		// TODO: implement this method
+		return null;
+	}
+
+	/**
+	 * Return true if there is a direct flight from 'fromCountry' to
+	 * 'toCountry'. Otherwise, return false.
+	 */
+	public boolean isDirectFlight(String fromCountry, String toCountry) {
+		// TODO: implement this method
+		return false;
+	}
+
+	/* Exercise 2 methods */
+	/**
+	 * Return all the countries that are reachable from the given country, using
+	 * any number of flights (for example, if we can fly from A to B, then from
+	 * B to C, then we can say that both B and C are reachable from A.
+	 */
+	public List<String> getReachableDestinations(String country) {
+		// TODO: implement this method
+		return null;
+	}
+
+	/* Exercise 3 methods */
+
+	/**
+	 * Return the country ('destinationA' or 'destinationB') which requires
+	 * fewer flights to travel to from country 'current'
+	 */
+	public String closerDestination(String current, String destinationA, String destinationB) {
+		// TODO: implement this method
+		return null;
+	}
+}
