3b491e195185076012277f2bd2aa832dafae16f2
bfindlay <bfin5330@uni.sydney.edu.au>
2016-09-15 01:32:42 +1000

    added more test for removal

--- Info1105/assignment1/EnhancedTreeTest.java
+++ Info1105/assignment1/EnhancedTreeTest.java
@@ -8,11 +8,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import sun.util.locale.LocaleUtils;
 
 public class EnhancedTreeTest {
 
@@ -130,6 +133,7 @@ public class EnhancedTreeTest {
 		calendar.add("Exam", d, "HOME");
 		calendar.add("Lunch", e, "Carslaw");
 		assertEquals(1, calendar.getAppointments("Carslaw").size());
+
 		Appointment app = calendar.getNextAppointment(e);
 		calendar.remove(app);
 
@@ -138,8 +142,24 @@ public class EnhancedTreeTest {
 		calendar.remove(app2);
 		assertEquals("Exam", calendar.getNextAppointment(c).getDescription());
 		// Check for null argument exception
+
+		calendar.add("Lunch", e, "Carslaw");
+		calendar.add("Lunch", e, "Carslaw");
+		calendar.add("Lunch", e, "Carslaw");
+
+		Appointment lunch = calendar.getNextAppointment(e);
+		assertEquals("Lunch", lunch.getDescription());
+
+		List<Appointment> lis = calendar.getAppointments("Carslaw");
+		assertEquals(3, lis.size());
+		calendar.remove(lunch);
+		System.out.println(lis.size());
+		assertEquals(2, lis.size());
+
 		thrown.expect(IllegalArgumentException.class);
 		calendar.remove(null);
+
+		// TODO remove appointment not in the list;
 
 		// TODO remove appointment not in the list;
 
@@ -303,6 +323,7 @@ public class EnhancedTreeTest {
 
 	}
 
+	// TODO check this
 	@Test
 	public void testRemoveAll() throws ParseException {
 
@@ -326,8 +347,194 @@ public class EnhancedTreeTest {
 		calendar.remove(app2);
 
 		Appointment app3 = calendar.getNextAppointment(a);
-		assertEquals("Exam3", app3.getDescription());
 
+	}
+
+	@Test
+	public void testInsertMany() throws ParseException {
+		Date a = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 08:00:00");
+		Date b = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 10:00:00");
+		Date c = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 15:00:00");
+		Date d = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/03 09:59:59");
+		Date e = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/03 12:00:00");
+
+		Calendar calendar = new EnhancedAssignment();
+
+		for (int i = 0; i < 100; i++) {
+			calendar.add("A", a, "Sydney");
+		}
+		for (int i = 0; i < 100; i++) {
+			calendar.add("B", b, "QLD");
+		}
+		for (int i = 0; i < 100; i++) {
+			calendar.add("C", c, "Melbourne");
+		}
+
+		List<Appointment> aList = calendar.getAppointments("Sydney");
+		List<Appointment> bList = calendar.getAppointments("QLD");
+		List<Appointment> cList = calendar.getAppointments("Melbourne");
+
+		assertEquals(100, aList.size());
+		assertEquals(100, bList.size());
+		assertEquals(100, cList.size());
+
+	}
+
+	@Test
+	public void testInsertRemoveMany() throws ParseException {
+
+		Date a = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 08:00:00");
+		Date b = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 10:00:00");
+		Date c = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 15:00:00");
+		Date d = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/03 09:59:59");
+		Date e = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/03 12:00:00");
+
+		Calendar calendar = new EnhancedAssignment();
+
+		calendar.add("A", e, "SIT");
+		Appointment app = calendar.getNextAppointment(a);
+		assertEquals("A", app.getDescription());
+		assertNotNull(app);
+		calendar.remove(app);
+		assertNull(calendar.getNextAppointment(e));
+
+		// Add elements to the calendar
+		for (int i = 0; i < 20; i++) {
+			calendar.add("A", a, "Sydney");
+		}
+
+		List<Appointment> list = calendar.getAppointments("Sydney");
+		assertEquals(20, list.size());
+
+		Appointment a20 = list.get(19);
+
+		list.stream().forEach(en -> {
+			calendar.remove(en);
+		});
+
+		assertEquals(0, list.size());
+		// Appointment a0 = list.get(0);
+		// Appointment a1 = list.get(1);
+		// Appointment a2 = list.get(2);
+		// Appointment a3 = list.get(3);
+		// Appointment a4 = list.get(4);
+		// Appointment a5 = list.get(5);
+		// Appointment a6 = list.get(6);
+		// Appointment a7 = list.get(7);
+		//
+		// calendar.remove(a0);
+		// calendar.remove(a1);
+		// calendar.remove(a2);
+		// calendar.remove(a3);
+		// calendar.remove(a4);
+		// calendar.remove(a5);
+		// calendar.remove(a6);
+		// calendar.remove(a7);
+		//
+		// list.stream().forEach(element -> {
+		// System.out.println("deleted");
+		// calendar.remove(element);
+		// });
+
+	}
+	// Date time = appointment.getStartTime();
+	// String loc = appointment.getLocation();
+	// TreeSet<Appointment> list = tree.get(time).get(loc);
+	// if (tree.get(time).size() == 1) {
+	// tree.remove(time);
+	// return;
+	// }
+	// if (list != null) {
+	// list.remove(appointment);
+	// }
+
+	@Test
+	public void testInsertionInOrder() throws ParseException {
+		Date a = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 08:00:00");
+		Date b = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 10:00:00");
+		Date c = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 15:00:00");
+		Date d = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/03 09:59:59");
+		Date e = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/03 12:00:00");
+
+		Calendar calendar = new EnhancedAssignment();
+
+		calendar.add("A", a, "A");
+		calendar.add("A", a, "A");
+		calendar.add("B", b, "B");
+		calendar.add("B", b, "B");
+		calendar.add("B", b, "B");
+		calendar.add("B", b, "B");
+
+		assertEquals(2, calendar.getAppointments("A").size());
+		assertEquals(4, calendar.getAppointments("B").size());
+
+	}
+
+	@Test
+	public void testDeletionInOrder() throws ParseException {
+		Date a = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 08:00:00");
+		Date b = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 10:00:00");
+		Date c = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 15:00:00");
+		Date d = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/03 09:59:59");
+		Date e = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/03 12:00:00");
+
+		Calendar calendar = new EnhancedAssignment();
+
+		calendar.add("A", a, "A");
+		calendar.add("A", a, "A");
+		calendar.add("B", b, "B");
+		calendar.add("B", b, "B");
+		calendar.add("B", b, "B");
+		calendar.add("B", b, "B");
+		calendar.add("C", c, "C");
+
+		assertEquals(2, calendar.getAppointments("A").size());
+		assertEquals(4, calendar.getAppointments("B").size());
+		assertEquals(1, calendar.getAppointments("C").size());
+
+		Appointment A = calendar.getNextAppointment(a);
+		Appointment B = calendar.getNextAppointment(b);
+		Appointment C = calendar.getNextAppointment(c);
+
+		calendar.remove(A);
+		assertEquals(1, calendar.getAppointments("A").size());
+		Appointment A2 = calendar.getNextAppointment(a);
+		calendar.remove(A2);
+		assertEquals(0, calendar.getAppointments("A").size());
+
+		calendar.remove(B);
+		assertEquals(3, calendar.getAppointments("B").size());
+
+	}
+
+	@Test
+	public void testgetNextRemoveAll() throws ParseException {
+		Date a = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 08:00:00");
+		Date b = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 10:00:00");
+		Date c = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/02 15:00:00");
+		Date d = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/03 09:59:59");
+		Date e = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2010/01/03 12:00:00");
+
+		Calendar calendar = new EnhancedAssignment();
+
+		calendar.add("A", a, "A");
+
+		Appointment A = calendar.getNextAppointment(a);
+		List<Appointment> ls = calendar.getAppointments("A");
+		assertEquals(1, ls.size());
+
+		calendar.remove(A);
+		assertNull(calendar.getNextAppointment(a));
+
+		assertEquals(0, calendar.getAppointments("A").size());
+
+		calendar.getAppointments("A");
+		Appointment appt = calendar.getNextAppointment(df.parse("1970/01/01 00:00:00"));
+
+		while (appt != null) {
+			calendar.remove(appt);
+			appt = calendar.getNextAppointment(a);
+		}
 	}
 
 }
